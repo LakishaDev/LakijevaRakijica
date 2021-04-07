1f0e846e0b130d04e6d9704c7d7db7b221f0a95e
Lazar-cyber <68185793+Lazar-cyber@users.noreply.github.com>
2021-04-07 13:25:23 +0200

    Update client.lua

--- lakijeva_rakijica/client.lua
+++ lakijeva_rakijica/client.lua
@@ -1,5 +1,6 @@
 -- !! NE DIRAJ NISTA AKO NE ZNAS !! --
 ESX = nil
+
 Citizen.CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
