d042e2bfe914a0d74a4f1086b50133c0554a4a8f
Lazar-cyber <68185793+Lazar-cyber@users.noreply.github.com>
2021-04-07 13:22:00 +0200

    Update server.lua

--- lakijeva_rakijica/server.lua
+++ lakijeva_rakijica/server.lua
@@ -1,6 +1,5 @@
 ------------------ OVDE NE PIPAJ NISTA --------------------------
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
 ESX.RegisterUsableItem('rakijica', function(source)
     local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
