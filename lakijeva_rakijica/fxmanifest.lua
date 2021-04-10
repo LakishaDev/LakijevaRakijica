4f85a15cda5d35128957d30a7dc044661cdac9b7
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-04-10 10:47:19 +0100

    Nova verzija manifesta :)

--- lakijeva_rakijica/fxmanifest.lua
+++ lakijeva_rakijica/fxmanifest.lua
@@ -1,7 +1,9 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+-- Resource Verzija
+fx_version 'cerulean'
+game 'gta5'
 
 author "L4K1SH4#7855 on Discord"
-
+description 'LakijevaRakijica xD'
 version "1.0.2"
 
 client_scripts {
