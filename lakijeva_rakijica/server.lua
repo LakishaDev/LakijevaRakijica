566bae035882007d448266aac981812a04fb0bf7
Lakisha <68185793+LakishaDev@users.noreply.github.com>
2022-02-03 14:05:51 +0100

    Update server.lua

--- lakijeva_rakijica/server.lua
+++ lakijeva_rakijica/server.lua
@@ -2,8 +2,8 @@ if Config.KoristiESX then
 	local ESX = nil
 	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 	ESX.RegisterUsableItem('rakijica', function(source)
-	    local _source = source
-		local xPlayer = ESX.GetPlayerFromId(_source)
-		TriggerClientEvent('lakijeva-rakija:efektRakije', _source)
+	    local src = source
+		local xPlayer = ESX.GetPlayerFromId(src)
+		TriggerClientEvent('lakijeva-rakija:efektRakije', src)
 	end)
 end
